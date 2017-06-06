80fe669a69396c8151df895b435755d4f953f626
Nayna Pashilkar <nayna.pashilkar@gmail.com>
2017-06-06 15:24:28 +0000

    silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,8 @@
 #code your solutions here
-
-
-
+my_word = "yeet"
+puts my_word.upcase
+puts "IM NOT SHOUTING".downcase
+name = "Karlie Kloss"
+puts name.length 
+puts "wow " + "mom"
+puts "i really like programming".capitalize
